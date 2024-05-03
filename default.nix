14a98d69f86cb07aa8945f0e784ee38693a68e53
Lucas Denefle <lucas.denefle@converge.io>
2024-05-03 18:25:12 +0100

    chore: format nix files

--- default.nix
+++ default.nix
@@ -4,9 +4,7 @@ stdenv.mkDerivation {
   pname = "mixx-m3u-loader";
   version = "0.0.1";
 
-  propagatedBuildInputs = [
-    python3
-  ];
+  propagatedBuildInputs = [ python3 ];
 
   dontUnpack = true;
 
@@ -14,8 +12,7 @@ stdenv.mkDerivation {
     "install -Dm755 ${./mixxx-m3u-loader} $out/bin/mixx-m3u-loader";
 
   meta = with lib; {
-    description =
-      "Small package that loads m3u files as mixxx playlist";
+    description = "Small package that loads m3u files as mixxx playlist";
     maintainers = with maintainers; [ ldenefle ];
   };
 }
--- flake.nix
+++ flake.nix
@@ -5,15 +5,15 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      mixxx-m3u-loader = pkgs.callPackage ./. {};
-    in
-      {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mixxx-m3u-loader = pkgs.callPackage ./. { };
+      in {
+        formatter = pkgs.nixfmt;
+
         packages = {
           mixxx-m3u-loader = mixxx-m3u-loader;
           default = mixxx-m3u-loader;
         };
-      }
-    );
+      });
 }
